a12b206678f8d53df8473df86d9611960f571081
diegosfc100 <diego.sfc100@hotmail.com>
2021-06-28 12:40:03 -0300

    Condicional IF, ELSE

---
+++ 04.Estruturas de Controle/02.Condicional IF ELSE.js
@@ -0,0 +1,13 @@
+// IF, ELSE
+
+const imprimirResultado = function(nota){
+    if (nota >= 7) {
+        console.log('Aprovado!')
+    } else {
+        console.log('Reprovado!')
+    }
+}
+
+imprimirResultado(10) // Aprovado!
+imprimirResultado(4) // Reprovado!
+imprimirResultado('Epa') // Reprovado!
