9e7257a5494dfa546e182c10d653494c5330e1ac
diegosfc100 <diego.sfc100@hotmail.com>
2021-07-01 23:23:57 -0300

    Tipos de Declaração

---
+++ 05.Funcoes/12.Tipos de Declaracao.js
@@ -0,0 +1,16 @@
+// Somente a function declaration consegue ser executado antes da declaração
+
+//Function declaration
+function soma(x,y) {
+    return x + y
+}
+
+//Function expression
+const sub = function(x,y) {
+    return x - y
+}
+
+//Named function expression
+const mult = function mult(x,y) {
+    return x * y
+}
