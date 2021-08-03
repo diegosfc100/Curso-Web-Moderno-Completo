cb4b3757fa32a677ae8b38bf18208f63c39a307e
diegosfc100 <diego.sfc100@hotmail.com>
2021-08-03 20:38:23 -0300

    Herança part III

---
+++ 06.Objeto/09.Heranca part III.js
@@ -0,0 +1,25 @@
+const pai = { nome: 'Pedro', corCabelo: 'preto' };
+
+const filha1 = Object.create(pai);
+filha1.nome = 'Ana';
+
+console.log(filha1.corCabelo); // preto
+
+const filha2 = Object.create(pai, {
+  nome: { value: 'Bia', writable: false, enumerable: true },
+});
+
+console.log(filha2.nome); // Bia
+
+filha2.nome = 'Carla';
+console.log(`${filha2.nome} tem cabelo ${filha2.corCabelo}`); // Bia tem cabelo preto
+
+console.log(Object.keys(filha1)); // [ 'nome' ]
+console.log(Object.keys(filha2)); // [ 'nome' ]
+
+
+for (const key in filha2) {
+  filha2.hasOwnProperty(key)
+    ? console.log(key) // nome
+    : console.log(`Por herança: ${key}`); // Por herança: corCabelo
+}
